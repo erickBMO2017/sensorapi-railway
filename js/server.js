63baaeca5e4c23c8879facc2c23315081ea1aa72
erickBMO2017 <erick.araque.bmo@gmail.com>
2025-08-07 09:01:35 -0400

    Cambio de puerto

--- js/server.js
+++ js/server.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
